6f43cac40d63f6d19cf09b27945b9ea29f12bec2
Mike Molinari <mike@smbisoft.com>
2022-03-08 03:40:18 +0000

    updating login system for openSCAD.cloud

---
+++ openscad/login.js
@@ -0,0 +1,7 @@
+
+function doLogin() {
+    window.open('https://autodrop.cloud/?&CurrentBodyDiv=loginFromCAD&cadLoginOriginURL=openscad.cloud&currentDivs=parts&loginFromCAD', 'popup', 'width=600,height=600'); 
+    return false;
+}
+
+
